849e83c4f6a82f671fc4db87925aa600311624f1
Alexandr Kolganov <gengizar@yandex.ru>
2023-03-22 23:14:45 +0500

    docs(readme): initial readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // need to change BrowserRouter to HashRouter for gh-pages
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Favicon from 'react-favicon';
 import favicon from './img/Book_25711.ico';
 
@@ -11,12 +11,12 @@ import Page404 from './components/Page404/Page404';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Favicon url={favicon} />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="book/:id" element={<BookInfo />} />
       <Route path="*" element={<Page404 />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
